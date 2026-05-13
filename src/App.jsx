a5a5b452cd6d7a4ae7e7b803e5fb14aa43da9986
edupoly <info.edupoly@gmail.com>
2026-05-13 07:44:03 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-danger">Hello Edupoly</h1>
-      <h2>Hi Praveen</h2>
+    <div className="border border-5 border-success m-2 p-2">
+      <h1>Hello App</h1>
     </div>
   );
 }
